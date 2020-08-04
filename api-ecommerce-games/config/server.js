e72838afcc7cf17bddf5b032d31e2eb67334da03
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-08-04 12:44:05 -0300

    initial commit

---
+++ api-ecommerce-games/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c543ef0423b823b9f44cb90777b3dde'),
+    },
+  },
+});
---
+++ api-ecommerce-games/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '506567a0-7646-4a46-bcf7-cee38734ac55'
+};
