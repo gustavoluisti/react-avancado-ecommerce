160607f4454554b02fbc95cbaeb45ae8c43d4a55
Luis Gustavo <luis.gustavo@qintess.com>
2020-08-28 21:13:00 -0300

    Variaveis de ambiente

--- landing-page/src/components/Logo/index.tsx
+++ landing-page/src/components/Logo/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import * as S from './styles'
 import { LogoProps } from 'types/api'
 
-const Logo = ({alternativeText, url }: LogoProps) => <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+import { getImageUrl } from 'utils/getImageUrl'
+
+const Logo = ({alternativeText, url }: LogoProps) => <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 
 export default Logo
