7a4fec26b678b438d6d5de5290fc7addc8cf622a
Luis Gustavo <gustavoluisrh@gmail.com>
2020-09-27 16:54:15 -0300

    Trabalhando com arquivos .env

--- frontend/src/components/Logo/index.tsx
+++ frontend/src/components/Logo/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
 const Logo = ({alternativeText, url}: LogoProps) => 
-  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
+  <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 
 export default Logo
