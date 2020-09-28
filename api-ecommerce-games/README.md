b67083af8e6cb2cfe4be6c82db0cace5e9816aff
Luis Gustavo <gustavoluisrh@gmail.com>
2020-09-27 21:40:22 -0300

    Ajustes

--- api-ecommerce-games/README.md
+++ api-ecommerce-games/README.md
@@ -1,3 +1,5 @@
 # Strapi application
 
 A quick description of your strapi application
+
+docker run --name strapi -e POSTGRES_PASSWORD=docker  -p 5432:5432 -d postgres 
--- api-ecommerce-games/config/database.js
+++ api-ecommerce-games/config/database.js
@@ -6,10 +6,10 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5434),
+        port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        password: env("DATABASE_PASSWORD", "docker"),
         ssl: env.bool("DATABASE_SSL", false),
       },
       options: {},
